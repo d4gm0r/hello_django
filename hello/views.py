254c6857b132d03a8da95c328fbf6e409627e8b4
Nathan Larsen <d4gm0r@gmail.com>
2017-04-03 02:42:54 +0000

    added a timestamp

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 def index(request):
-    return HttpResponse("Hello Django World!!")
+    return HttpResponse("Hello Django World!! You're here at: " + time.strftime("%c"))
 
 # Create your views here.
-
